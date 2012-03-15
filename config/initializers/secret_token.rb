90909c13338cf2e27776b16909dbae3563b932a0
Eivind Hagen <eivind@sibblingz.com>
2012-03-14 20:43:58 -0700

    Chameleon initial Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chameleon::Application.config.secret_token = '3d845800d9fc534d1835ee611e7b3ce17fff01eb4b54c92a360b4d6c7636e3ed449c4c868a29c3b42d352dedbc6f0fee9fc353ce4a273f3cede9a1d2744f65f9'
